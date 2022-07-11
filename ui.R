48286753a2ec8d8ebccaf3874461ea67056bf369
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2022-07-11 20:34:24 +0100

    Add files via upload

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@
 
 list.of.packages<-c("shiny","shinyWidgets","shinyjs","shinyBS",
                     "ggplot2","tidyr","tools",
-                    "mnormt","mle4","MuMln",
+                    "mnormt","mle4","MuMIn",
                     "readxl","writexl","car","stringr","clipr","SuppDists","e1071","pracma"
                     )
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -27,7 +27,7 @@ library(tools)       # system
 # library(gridExtra)   # not sure any longer
 library(mnormt)      # pmnorm for logistic
 library(lme4)        # lmer (mixed models)
-library(MuMln)       # r-squared for mixed models
+library(MuMIn)       # r-squared for mixed models
 library(readxl)      # excel
 library(writexl)     # x excel
 library(car)         # Anova type 3 correct
